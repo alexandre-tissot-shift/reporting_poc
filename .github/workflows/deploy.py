8a88e45cd6ef9b74b9a6c4cc7e122476494a1ebc
Alexandre Tissot <126464183+alexandre-tissot-shift@users.noreply.github.com>
2024-10-04 14:41:41 +0200

    Create deploy.py

---
+++ .github/workflows/deploy.py
@@ -0,0 +1,37 @@
+from gooddata_sdk import GoodDataSdk, CatalogWorkspace
+
+# GoodData host in the form of uri eg. ""
+host = "https://scholarly-emu.trial.cloud.gooddata.com"
+# GoodData API token
+token = "Y2M5ZDk2OTUtMTY5Zi00OTNkLWI3MDMtZGFiZTQ5NWVkNDU4OkFQSV9Ub2tlbjp2THhVRjJHNVJQcWQ4dG4xNU1Zb2VmM0Y3bkNsTXhlQQ=="
+demo_workspace_id = "9fa384e3a6e143d68d64f0af37f82f7a"
+production_workspace_id = "884d06081613422db73daa3c8904cd38"
+
+sdk = GoodDataSdk.create(host, token)
+
+# Create workspace
+sdk.catalog_workspace.create_or_update(
+  CatalogWorkspace(production_workspace_id, production_workspace_id)
+)
+
+# Get LDM (Logical Data Model) from demo workspace
+declarative_ldm = sdk.catalog_workspace_content.get_declarative_ldm(
+  demo_workspace_id
+)
+
+# Get analytics model (metrics, dashboards, etc.) from demo workspace
+declarative_analytics_model = sdk.catalog_workspace_content.get_declarative_analytics_model(
+  demo_workspace_id
+)
+
+# Put LDM (Logical Data Model) to production workspace
+sdk.catalog_workspace_content.put_declarative_ldm(
+  production_workspace_id,
+  declarative_ldm
+)
+
+# Put analytics model (metrics, dashboards, etc.) to production workspace
+sdk.catalog_workspace_content.put_declarative_analytics_model(
+  production_workspace_id,
+  declarative_analytics_model
+)
